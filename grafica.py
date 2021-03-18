d4b45c3a5d085003d3e12afda1574f0a9d8297b4
pedro-hdez <pedro.a.hdez.a@gmail.com>
2021-03-18 16:46:43 -0700

    Estructuras de datos separadas de la clase grafica

--- grafica.py
+++ grafica.py
@@ -1,14 +1,7 @@
-"""
-    Este archivo implementa los nodos como una clase con
-    atributos.
-    Se le hicieron las modificaciones correspondientes a 
-    la clase 'Grafica' para que maneje los nodos de esta
-    nueva forma
-"""
-
 import copy
 import operator
 import math
+from estructuras_datos import *
 
 class Arista:
     """
@@ -21,6 +14,7 @@ class Arista:
         self.destino = destino
         self.peso = peso
         self.etiqueta = etiqueta
+#----------------------------------------------------------------
 
 class Nodo:
     """
@@ -32,82 +26,7 @@ class Nodo:
         self.etiqueta = etiqueta
         self.grado = 0
 
-#----------------------------------------------------------------
-class Cola:
-    """ Representa a una cola, con operaciones de encolar y desencolar.
-        El primero en ser encolado es también el primero en ser desencolado.
-    """
 
-    def __init__(self):
-        """ Crea una cola vacía. """
-        # La cola vacía se representa por una lista vacía
-        self.items=[]
-
-    def imprimir_cola(self):
-        for i in range(len(self.items)):
-            print(self.items[i])
-
-    def encolar(self, x):
-        """ Agrega el elemento x como último de la cola. """
-        self.items.append(x)
-    def es_vacia(self):
-        """ Devuelve True si la cola esta vacía, False si no."""
-        return self.items == []
-      
-    def desencolar(self):
-        """ Elimina el primer elemento de la cola y devuelve su
-        valor. Si la cola está vacía, levanta ValueError. """
-        try:
-            return self.items.pop(0)
-        except:
-            raise ValueError("La cola está vacía")
-    
-    def vaciar(self):
-        self.items = []
-        
-
-#----------------------------------------------------------------
-class Pila:
-    """ Representa una pila con operaciones de apilar, desapilar y
-        verificar si está vacía. """
-
-    def __init__(self):
-        """ Crea una pila vacía. """
-        # La pila vacía se representa con una lista vacía
-        self.items=[]
-        
-    def apilar(self, x):
-        """ Agrega el elemento x a la pila. """
-        # Apilar es agregar al final de la lista.
-        self.items.append(x)
-
-    def es_vacia(self):
-    	return self.items == []
-
-    def desapilar(self):
-        """ Elimina el primer elemento de la cola y devuelve su
-        valor. Si la cola está vacía, levanta ValueError. """
-        try:
-            return self.items.pop()
-        except:
-            raise ValueError("La cola está vacía")
-    
-    def vaciar(self):
-        self.items = []
-#----------------------------------------------------------------
-class UnionBusqueda:
-    def __init__(self, vertices):
-        self.__items = {}
-        for v in vertices:
-            self.__items[v] = v
-    
-    def busqueda(self, v):
-        if (v == self.__items[v]):
-            return v
-        return self.busqueda(self.__items[v])
-    
-    def union(self, u, v):
-        self.__items[self.busqueda(u)] = self.busqueda(v)
 #----------------------------------------------------------------
 class Grafica:
     """
